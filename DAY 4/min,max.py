b0036ebcddfdcb96c17423a4fe62a014f7396294
__Arijit Das__ <132340245+ArijitDas2005@users.noreply.github.com>
2024-08-12 22:56:35 +0530

    Update min,max.py

---
+++ DAY 4/min,max.py
@@ -0,0 +1,8 @@
+cities_population = {"Albuquerque":559121,"Tulsa":403505}
+my_list = [5**5, 12**2, 3050, 475*2]
+
+print(min(cities_population.keys()))
+
+print(max(cities_population.values()))
+
+print(max(my_list))
