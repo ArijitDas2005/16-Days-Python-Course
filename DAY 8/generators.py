c3bb78a9d1e6703b2783a13a09feb86e900d9168
__Arijit Das__ <132340245+ArijitDas2005@users.noreply.github.com>
2024-08-18 08:30:58 +0530

    Add files via upload

---
+++ DAY 8/generators.py
@@ -0,0 +1,53 @@
+# def my_function():
+#     return 4
+#
+#
+# def my_generator():
+#     yield 4
+#
+#
+# print(my_function())
+# print(my_generator())
+# print(next(my_generator()))
+
+
+
+# def my_function():
+#     my_list=[]
+#     for x in range (1,5):
+#         my_list.append(x*10)
+#     return my_list
+#
+#
+# def my_generator():
+#     for x in range (1,5):
+#         yield x * 10
+#
+#
+# print(my_function())
+# print(my_generator())
+#
+# g= my_generator()
+#
+# print(next(g))
+# print(next(g))
+
+
+
+# def my_generator():
+#
+#     x=1
+#     yield x
+#
+#     x+=1
+#     yield x
+#
+#     x+=1
+#     yield x
+#
+#
+# g = my_generator()
+#
+# print(next(g))
+# print(next(g))
+# print(next(g))
